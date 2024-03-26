e236947bbcefa33bd0ec61c3d4f1797e22b8ce72
gauravgadakh02 <gauravgadakh2017@gmail.com>
2024-03-26 16:29:36 +0530

    Your commit message

---
+++ src/components/Heading.jsx
@@ -0,0 +1,27 @@
+// import Tagline from "./Tagline"
+// const Heading = ({ className, title, text , tag}) => {
+//     return (
+//         <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`} >
+//             {tag && <Tagline>{tag}</Tagline>}
+//             {title && <h2 className="h2">{title}</h2>}
+//         {text && <p className="body-2 mt-4 text-n-4"> </p>}
+//         </div>
+//     )
+// }
+
+// export default Heading
+
+
+import TagLine from "./Tagline";
+
+const Heading = ({ className, title, text, tag }) => {
+    return (
+        <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20 md:text-center`}>
+            {tag && <TagLine className="mb-4 md:justify-center">{tag}</TagLine>}
+            {title && <h2 className="h2">{title}</h2>}
+            {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
+        </div>
+    );
+};
+
+export default Heading;
